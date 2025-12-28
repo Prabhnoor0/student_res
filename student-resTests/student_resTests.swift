2578f007671c748624afd002d39fad1edfa26452
Prabhnoor0 <prabhnooorkaur11@gmail.com>
2025-12-28 14:20:00 +0530

    Add Tests target

---
+++ student-resTests/student_resTests.swift
@@ -0,0 +1,17 @@
+//
+//  student_resTests.swift
+//  student-resTests
+//
+//  Created by Prabhnoor Kaur on 11/10/25.
+//
+
+import Testing
+@testable import student_res
+
+struct student_resTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
